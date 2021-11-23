601738c8abba637c03aa1c9ea952e9164f79614b
Hiroshi Matsuda <hiroshi_matsuda@megagon.ai>
2021-11-23 10:00:27 +0900

    ginza-transformers.TransformerModel.v3

--- ginza_transformers/architectures.py
+++ ginza_transformers/architectures.py
@@ -3,5 +3,5 @@ from spacy_transformers.util import registry
 
 
 registry.architectures.register(
-    "ginza-transformers.TransformerModel.v1", func=TransformerModelCustom
+    "ginza-transformers.TransformerModel.v3", func=TransformerModelCustom
 )
